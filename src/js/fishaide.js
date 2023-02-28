d1c1ba5fbfc1e9e66d77703496aa971782137e67
jacob Calderon <Jacob.a.calderon@gmail.com>
2023-02-28 14:06:36 -0600

    creating header and tab/search bar

---
+++ src/js/fishaide.js
@@ -0,0 +1,29 @@
+
+// <html>
+//   <head>
+//     <title>Header Bar with Search Feature</title>
+//     <style>
+//       #header-bar {
+//         width: 100%;
+//         background-color: #ccc;
+//         padding: 10px;
+//       }
+//       #search-box {
+//         width: 100%;
+//         padding: 10px;
+//       }
+//     </style>
+      
+//   </head>
+      
+//   <body>
+//     <div id="header-bar">
+//       <input type="text" id="search-box" placeholder="Search...">
+//     </div>
+//     <script>
+//       document.getElementById('search-box').addEventListener('keyup', function(e) {
+//         // code to search here
+//       });
+//     </script>
+//   </body>
+// </html>

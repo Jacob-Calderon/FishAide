1fa19b23806b9ddf4d4617912bd3e232563dda4f
jacob Calderon <Jacob.a.calderon@gmail.com>
2023-03-04 21:18:27 -0600

    updating html

--- src/js/web-scraper.js
+++ src/js/web-scraper.js
@@ -4,17 +4,18 @@ const cheerio = require('cheerio');
 const url = 'https://tpwd.texas.gov/fishboat/fish/action/reptmap.php?EcoRegion=HC';
 
 request(url, (error, response, html) => {
-  if (!error && response.statusCode == 200) {
+  if (!error && response.statusCode === 200) {
     const $ = cheerio.load(html);
 
     $('.result').each((i, el) => {
-      const waterCondition = $(el).find('.waterCondition').text().trim();
-      const fishCaught = $(el).find('.fishCaught').text().trim();
+      const bass = $(el).find('.waterCondition').text().trim();
+      const  catfish= $(el).find('.fishCaught').text().trim();
       const baitUsed = $(el).find('.baitUsed').text().trim();
       const location = $(el).find('.location').text().trim();
       const rating = $(el).find('.rating').text().trim();
 
-      console.log({waterCondition, fishCaught, baitUsed, location, rating});
+
+      console.log({bass, catfish, baitUsed, location, rating});
     });
   }
 });

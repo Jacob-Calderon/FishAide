c1f757b2b83741d76f04c997a12a42730be50444
jacob Calderon <Jacob.a.calderon@gmail.com>
2023-03-10 12:07:51 -0600

    looking at more webscrapers

---
+++ src/js/puppeteerscraper.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const searchGoogle = async (query) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.google.com/search?q=${query}`);
+};
+
+const searchGoogle = async (query) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.google.com/search?q=${query}`);
+
+    const data = await page.evaluate(() => {
+        const results = [];
+        const links = document.querySelectorAll('.g .rc .r a');
+        const titles = document.querySelectorAll('.g .rc .r a h3');
+
+        for (let i = 0; i < links.length; i++) {
+            results.push({
+                title: titles[i].innerText,
+                url: links[i].href
+            });
+        }
+
+        return results;
+    });
+
+    console.log(data);
+
+    await browser.close();
+};
